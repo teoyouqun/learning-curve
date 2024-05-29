46b8935a5e3142830fa87d2acd07481b36791d12
tyouqun@dso_WSL <tyouqun@dso.org.sg>
2024-05-29 14:17:53 +0800

    [LC] 383

---
+++ LeetCode/383_ransomNote/main.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char, int> dict;
+        for (auto c:magazine) {
+            if (!dict.contains(c)) dict[c] = 1;
+            else dict[c]++;
+        }
+        for (auto d:ransomNote) {
+            if (dict.contains(d) && dict[d] >= 1) {
+                dict[d]--;
+                continue;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
